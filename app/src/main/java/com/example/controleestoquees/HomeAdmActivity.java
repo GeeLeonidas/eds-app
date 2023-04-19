21805e65c8ef3c9b756b55f4d454be94870311c3
ferreira-caio <130701444+ferreira-caio@users.noreply.github.com>
2023-04-19 18:02:40 -0300

    add ui home adm, muda ui home funcionário

---
+++ app/src/main/java/com/example/controleestoquees/HomeAdmActivity.java
@@ -0,0 +1,14 @@
+package com.example.controleestoquees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeAdmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_adm);
+    }
+}
